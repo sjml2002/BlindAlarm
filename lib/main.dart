6e3999deda10f40dd6b4c8913d182959d60bbeae
sjml2002 <sjml2002@naver.com>
2021-11-24 15:12:40 +0900

    BLE연결정보 랜더링, 디자인 약간 수정

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blind_alarm/serial_manager.dart';
 import 'package:flutter/material.dart';
 import 'dart:developer';
 
+import './library.dart' as CtLib;
 import './alarm_setting.dart';
 //////// import ////////
 
@@ -44,19 +45,49 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   var timeData;
+  Map<String, dynamic> btConn = {
+    "conn": null,
+    "name": null,
+    "address": "",
+  };
 
-  void timeSetting() async {
-    final result = await Navigator.pushNamed(context, '/AlarmSetting');
-    log(result.toString());
-    setState(() {
-      timeData = result;
-    });
+  void timeSetting(BuildContext context) async {
+    log(btConn["conn"].toString());
+    if (btConn["conn"] == null) {
+      CtLib.ctAlert(context, "블루투스 연결을 먼저 해주세요.");
+    } else {
+      final result = await Navigator.pushNamed(context, '/AlarmSetting',
+          arguments: btConn);
+      log(result.toString());
+      if (result != null) {
+        setState(() {
+          timeData = result;
+        });
+      }
+    }
   }
 
   void serialSetting() async {
-    final result = await Navigator.pushNamed(context, '/SerialSetting');
-    log(result.toString());
-    log("블루투스 연결");
+    var btResult = await Navigator.pushNamed(context, '/SerialSetting');
+    //btResult == null이면 그냥 뒤로가기 누른 것임
+    if (btResult != null) {
+      btResult = btResult as Map<String, dynamic>;
+      btConn["conn"] = btResult["conn"];
+      btConn["name"] = btResult["deviceName"];
+      btConn["address"] = btResult["deviceAddress"];
+      setState(() {}); //how to setState with map
+      log("블루투스 연결 정보: ${btConn["name"]}"); //DEBUG
+    }
+  }
+
+  void serialDisconnect() {
+    if (btConn["conn"] != null) {
+      btConn["conn"].close(); //disconnect
+    }
+    btConn["conn"] = null;
+    btConn["name"] = null;
+    btConn["address"] = "";
+    setState(() {});
   }
 
   @override
@@ -66,39 +97,68 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+          // 알람 View //
           Expanded(
-            child: Row(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text("알람: $timeData"),
-                // 알람 설정 버튼 //
                 Container(
-                    padding: const EdgeInsets.only(bottom: 8),
+                  padding: const EdgeInsets.only(bottom: 15),
+                  child: Text("알람: $timeData"),
+                ),
+                Container(
+                    padding: const EdgeInsets.only(bottom: 5),
                     child: TextButton(
                       style: ButtonStyle(
                         foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.red),
+                            MaterialStateProperty.all<Color>(Colors.purple),
                       ),
                       onPressed: () {
-                        timeSetting();
+                        timeSetting(context);
                       },
                       child: const Text('알람설정'),
                     )),
-                // 블루투스 연결 버튼 //
-                Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: TextButton(
-                      style: ButtonStyle(
-                        foregroundColor:
-                            MaterialStateProperty.all<Color>(Colors.blue),
-                      ),
-                      onPressed: () {
-                        serialSetting();
-                      },
-                      child: const Text('블루투스 연결'),
-                    ))
               ],
             ),
+          ),
+          // 블루투스 View //
+          Expanded(
+            flex: 1,
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              Container(
+                padding: const EdgeInsets.only(bottom: 15),
+                child: Text("블루투스 연결 정보: ${btConn["name"].toString()}"),
+              ),
+              Container(
+                  //connect Btn
+                  padding: const EdgeInsets.only(bottom: 3),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.blue),
+                    ),
+                    onPressed: () {
+                      serialSetting();
+                    },
+                    child: const Text('블루투스 연결'),
+                  )),
+              Container(
+                  //disconnect Btn
+                  padding: const EdgeInsets.only(bottom: 3),
+                  child: TextButton(
+                    style: ButtonStyle(
+                      foregroundColor:
+                          MaterialStateProperty.all<Color>(Colors.red),
+                    ),
+                    onPressed: () {
+                      serialDisconnect();
+                    },
+                    child: const Text('블루투스 연결 해제'),
+                  ))
+            ]),
           )
         ]),
       ),
